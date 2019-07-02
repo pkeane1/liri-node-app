e3fdafcfe2467ded6d527f12c77460bfe0727b93
Pierce Keane <Pierce.Keane@gmail.com>
2019-07-01 20:24:43 -0400

    saving

--- liri.js
+++ liri.js
@@ -1,18 +1,20 @@
 require("dotenv").config();
 
-var keys = require("keys.js");
+var keys = require("./keys.js");
 
 var Spotify = ("node-spotify-id")
 var spotify = (keys.spotify)
 
-var moment = require('moment');
-var axios = require('axios');
+var moment = require("moment");
+var axios = require("axios")
 
-var fs = require('fs');
+var fs = require("fs");
 
 //Make it so liri.js can take in one of the following commands:
 //switch statement swicth() -- case"" ---break
-
+// argv 2 will be the command and argv3 will be what I want to use with the command 
+// spotify - this - song ("argv2") sympathy of the devil (argv3) 
+console.log(process.argv)
 input1 = process.argv[2]
 input2 = process.argv[3]
 
@@ -32,8 +34,8 @@ break;
 };
 
 
-concertThis =function(input2) {
-
+function concertThis(input2) {
+console.log("it works!")
 } 
 
 spotifySong = function(input2) {
